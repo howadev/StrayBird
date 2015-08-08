752a624742174b1429c706ce99364bee0adce44c
rockleedev <rockleedev@gmail.com>
2015-08-08 13:30:05 -0400

    setup project

---
+++ chasingbird/chasingbird/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chasingbird
+//
+//  Created by Howard on 2015-08-08.
+//  Copyright © 2015 howalee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chasingbird/chasingbird/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chasingbird
+//
+//  Created by Howard on 2015-08-08.
+//  Copyright © 2015 howalee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
