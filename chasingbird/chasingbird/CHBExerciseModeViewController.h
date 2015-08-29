a061095db8976c905e7c800b78d7910dfbc400be
rockleedev <rockleedev@gmail.com>
2015-08-29 10:11:01 -0400

    exercise mode wip

---
+++ chasingbird/chasingbird/CHBExerciseModeViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHBExerciseModeViewController.h
+//  chasingbird
+//
+//  Created by Howard on 2015-08-27.
+//  Copyright © 2015 howalee. All rights reserved.
+//
+
+#import "CHBViewController.h"
+
+@interface CHBExerciseModeViewController : CHBViewController
+
+@end
