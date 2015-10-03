4d5868875e03293310d9e823b078e6cd33250a57
Howard Li <lihowadev@gmail.com>
2015-10-03 15:49:19 -0400

    add all files

---
+++ chasingbird/chasingBirdWatch Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  chasingBirdWatch Extension
+//
+//  Created by Rock Lee on 2015-10-03.
+//  Copyright © 2015 howalee. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
