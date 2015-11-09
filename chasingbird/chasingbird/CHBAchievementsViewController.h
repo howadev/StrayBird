acb8170806070b39b3df6ce82b4f0fe887d1d9f6
Howard Li <lihowadev@gmail.com>
2015-11-09 17:59:41 -0500

    CHBAchievementsViewController wip

---
+++ chasingbird/chasingbird/CHBAchievementsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CHBAchievementsViewController.h
+//  chasingbird
+//
+//  Created by Rock Lee on 2015-11-09.
+//  Copyright © 2015 howalee. All rights reserved.
+//
+
+#import "CHBViewController.h"
+
+@interface CHBAchievementsViewController : CHBViewController
+
+@end
---
+++ chasingbird/chasingbird/CHBAchievementsViewController.m
@@ -0,0 +1,23 @@
+//
+//  CHBAchievementsViewController.m
+//  chasingbird
+//
+//  Created by Rock Lee on 2015-11-09.
+//  Copyright © 2015 howalee. All rights reserved.
+//
+
+#import "CHBAchievementsViewController.h"
+
+@interface CHBAchievementsViewController ()
+
+@end
+
+@implementation CHBAchievementsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setupBackButton];
+}
+
+@end
